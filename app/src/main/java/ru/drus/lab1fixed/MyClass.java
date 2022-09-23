d7e5fc704cf65cd4935c20affcf0424ef911210c
Andrey Dubrovin <verssy236@gmail.com>
2022-09-23 23:28:43 +0700

    add javadoc

--- app/src/main/java/ru/drus/lab1fixed/MyClass.java
+++ app/src/main/java/ru/drus/lab1fixed/MyClass.java
@@ -1,12 +1,12 @@
 package ru.drus.lab1fixed;
 
 public class MyClass {
-    static int myMax(int x, int y) {
+    static public int myMax(int x, int y) {
         if (x > y)
             return x;
         return y;
     }
-    static int myMin(int x, int y) {
+    static public int myMin(int x, int y) {
         if (x < y)
             return x;
         return y;
---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.drus.lab1fixed","c":"MyClass","l":"MyClass()","url":"%3Cinit%3E()"},{"p":"ru.drus.lab1fixed","c":"MyClass","l":"myMax(int, int)","url":"myMax(int,int)"},{"p":"ru.drus.lab1fixed","c":"MyClass","l":"myMin(int, int)","url":"myMin(int,int)"}]
